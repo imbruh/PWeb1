f41c998f4c70cf7524b2fab3812272c08beeab4b
imbruh <brunaluiza112001@gmail.com>
2021-03-23 21:32:52 -0300

    app rodando com banco json

--- social-web-5-edicao-remocao-basico-flexlayout/src/app/shared/services/usuario.service.ts
+++ social-web-5-edicao-remocao-basico-flexlayout/src/app/shared/services/usuario.service.ts
@@ -1,22 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {USUARIOS} from '../model/USUARIOS';
+import { Observable } from 'rxjs';
 import {Usuario} from '../model/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-  usuarios: Array<Usuario>;
 
-  constructor() { 
-    this.usuarios = USUARIOS;
+  url_usuarios = 'http://localhost:3000/usuarios';
+
+  constructor(private httpClient: HttpClient){    
   }
 
-  listar(): Array<Usuario> {
-    return this.usuarios;
+  listar(): Observable<Usuario[]> {
+    return this.httpClient.get<Usuario[]>(this.url_usuarios);
   }
 
-  inserir(usuario: Usuario): void {
-    this.usuarios.push(usuario);
+
+  inserir(usuario: Usuario): Observable<Usuario> {
+   return this.httpClient.post<Usuario>(this.url_usuarios, usuario);
   }
 }
